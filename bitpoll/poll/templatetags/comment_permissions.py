9467f40e47e6928aea10e838ab814cfc73172880
bitbike <pshaube01.1994@googlemail.com>
2018-10-10 23:38:16 +0200

    update name

--- bitpoll/poll/templatetags/comment_permissions.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter
-def comment_can_edit(comment, user) -> bool:
-    return comment.can_edit(user)
--- bitpoll/poll/templatetags/poll_watched.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from ..models import Poll, BitpollUser, PollWatch
-
-register = template.Library()
-
-
-@register.filter(name="is_watching")
-def is_watching(user: BitpollUser, poll: Poll):
-    return poll.user_watches(user)
